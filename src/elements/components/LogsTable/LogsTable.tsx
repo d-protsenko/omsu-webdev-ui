a340d5f4307fd890f502150ec06e23b6766fb558
Danil Protsenko <dan.protsenko@gmail.com>
2020-10-11 20:55:38 +0600

    Added logs from table.

---
+++ src/elements/components/LogsTable/LogsTable.tsx
@@ -0,0 +1,48 @@
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import TableBody from '@material-ui/core/TableBody';
+import TableContainer from '@material-ui/core/TableContainer';
+import React from 'react';
+
+interface Log {
+  number: string;
+  timepoint: string;
+  message: string;
+}
+
+interface Props {
+  classes: any;
+  rows: Array<Log>;
+}
+
+const LogsTable: React.FC<Props> = ({ classes, rows }) => (
+  <TableContainer component={Paper} className={classes.container}>
+    <Table stickyHeader aria-label='sticky table'>
+      <TableHead>
+        <TableRow>
+          <TableCell width={'20px'}>#</TableCell>
+          <TableCell width={'100px'}>Time point</TableCell>
+          <TableCell align='left'>Log message</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {rows.map(row => (
+          <TableRow key={row.timepoint}>
+            <TableCell component='th' scope='row'>
+              {row.number}
+            </TableCell>
+            <TableCell component='th' scope='row'>
+              {row.timepoint}
+            </TableCell>
+            <TableCell align='left'>{row.message}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  </TableContainer>
+);
+
+export default LogsTable;
